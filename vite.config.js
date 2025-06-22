e179c5b6c5e408bf4883eb96714fa4b6ce054f40
tiagofort <tiagofortalezag@gmail.com>
2025-06-22 23:54:07 +0100

    deploying

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-const { defineConfig } = require('vite');
-const react = require('@vitejs/plugin-react');
+const { defineConfig } = require('vite')
+const react = require('@vitejs/plugin-react')
 
 module.exports = defineConfig({
   plugins: [react()],
-});
+  base: './',
+  build: {
+    outDir: 'dist'
+  }
+})
